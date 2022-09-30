d2512f8cdcdd9f185ab4ae5450731204c9917eb0
Isa Costa <isa-hcosta@hotmail.com>
2022-09-30 17:19:06 -0300

    requisito 6

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Table extends Component {
+class Table extends React.Component {
   render() {
     return (
-      <div>Table</div>
+      <table>
+        <tr>
+          <th> Descrição </th>
+          <th> Tag </th>
+          <th> Método de pagamento </th>
+          <th> Valor </th>
+          <th> Moeda </th>
+          <th> Câmbio utilizado </th>
+          <th> Valor convertido </th>
+          <th> Moeda de conversão </th>
+          <th> Editar/Excluir </th>
+        </tr>
+      </table>
     );
   }
 }
